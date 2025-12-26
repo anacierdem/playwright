6acc04facb6959819478d8ba9847726e5713ce6d
Ali Naci Erdem <anacierdem@gmail.com>
2025-12-26 20:26:56 +0000

    MacOS tests & cleanup

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Playwright devcontainer
 
-UI mode doesn't work, also see https://github.com/microsoft/playwright/issues/36958.
+Very basic Playwright configuration for running it in devcontainer.
+
+### Run tests:
+
+```shell
+npx playwright test
+```
+
+### Run a project in headed mode:
+
+```shell
+npx playwright test --headed
+```
+
+### Run UI mode:
+
+```shell
+npx playwright test --ui
+```
+
+If you are using an incompatible environment (see https://github.com/microsoft/playwright/issues/36958), you can simply run it on network;
+
+```shell
+npx playwright test --ui --ui-host 0.0.0.0 or --ui-port 9000
+```
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
@@ -25,9 +17,6 @@ export default defineConfig({
   reporter: [['html', { host: "0.0.0.0" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -40,6 +29,7 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
         launchOptions: {
           args: [
+            // This is required if your host is on Wayland
             "--ozone-platform=wayland"
           ],
         },
@@ -55,32 +45,5 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
